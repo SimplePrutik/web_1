2af6ed0101e90342172caefc103b1227db824c42
SimplePrutik <dimodaktil@yandex.ru>
2017-05-20 23:03:00 +0300

    added getting data, directive and factory

--- app/js/app.js
+++ app/js/app.js
@@ -1,30 +1,29 @@
 var app = angular.module('myApp', []);
-app.controller('CustomersReviews', function($scope, $http) {
-  $http.get("http://localhost:8000/reviews").then(function (response) {
-      $scope.review1 = (response.data[0]).review;
-      $scope.author1 = (response.data[0]).author;
-      $scope.job1 = (response.data[0]).job;
-      $scope.review2 = (response.data[1]).review;
-      $scope.author2 = (response.data[1]).author;
-      $scope.job2 = (response.data[1]).job;
-      $scope.review3 = (response.data[2]).review;
-      $scope.author3 = (response.data[2]).author;
-      $scope.job3 = (response.data[2]).job;
-  });
+
+app.factory("factory", function ($http) { 
+	this.get = function () { 
+		return $http.get('http://localhost:8000/reviews');
+	} 
+	return this; 
 });
 
-
-app.controller('PostReview', function($scope, $http){
-   $scope.PostData = function() {
-        $http({
-		    method: 'POST',
-		    url: 'http://localhost:8000/reviews',
-		    data: "review=" + $scope.review + "&author=" + $scope.author + "&job=" + $scope.job ,
-		    headers: {'Content-Type': "application/x-www-form-urlencoded"}
-		});
-        
-    }  
+app.directive("getData", function(factory) {
+    return {
+        link: function (scope) {
+          factory.get().then(function (response) {
+          	scope.comment = response.data;
+          });
+        }
+    };
 });
+
+app.controller('CustomersReviews', function($scope, factory) {
+	factory.get().then(function (response) {
+        $scope.data = response.data;
+    }); 
+})
+
+
 
 var app_route = angular.module('myAppRoute',["ngRoute"]);
 app_route.config(['$routeProvider', function($routeProvider) {
